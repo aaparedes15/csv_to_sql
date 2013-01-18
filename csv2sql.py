a899770a2102c6174675b5ef245fa1f2414a2743
aaparedes15 <ant.a.paredes@gmail.com>
2013-01-17 19:00:38 -0800

    main() has been written so that it is up to creating a blank table.

--- csv2sql.py
+++ csv2sql.py
@@ -19,4 +19,33 @@ def get_data_type(data):
     if "float"  in str(type(data)): return "REAL"
     if "int"    in str(type(data)): return "INTEGER"
 
+def main(csv_file):
+    """
+    The main method that will
+    take in the csv file and 
+    create a database and
+    a table.
+    """
+
+    # create the database name as same name
+    # as csv file with .db extension instead of
+    # .csv extension.
+    database =  csv_file[:-4] + ".db"
+
+    # create the database and cursor
+    con = sqlite3.connect(database)
+    cur = con.cursor()
+
+    # load in the data
+    data = csv2rec(csv_file)
+
+    # make the table
+    create_table_command = "CREATE TABLE {0} (".format(csv_file[:-4])
+    for n, t in zip(data.dtype.names, data[0]):
+        create_table_command += "{0} {1}, ".format(n, get_data_type(t))
+    create_table_command += ")"
+    cur.execute(create_table_command)
+
+
+
 
